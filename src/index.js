2e9770891edc2b70721140c8e496d1230f492c8f
Milauraaa <mila@stealth.design>
2022-10-13 12:21:48 +0100

    fix semantic layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 const Slide = () => {
-  //   const openBtn = document.querySelector(".slot-btn");
-  //   const burger = document.querySelector(".burger");
   const openBtn = document.querySelector("#open-btn");
   const closeBtn = document.querySelector("#cancel-btn");
   const formSection = document.querySelector(".form-section-frame");
@@ -26,39 +24,3 @@ const Slide = () => {
   });
 };
 Slide();
-
-// const openBtn = document.querySelector("#open-btn");
-// const modal = document.querySelector("#form-section");
-// const closeBtn = document.querySelector("#cancel-btn");
-
-// let disableScroll = function () {
-//   let pagePosition = window.scrollY;
-//   document.body.classList.add("disable-scroll");
-//   document.body.dataset.position = pagePosition;
-//   document.body.style.top = -pagePosition + "px";
-// };
-
-// let enableScroll = function () {
-//   let pagePosition = parseInt(document.body.dataset.position, 10);
-//   document.body.style.top = "auto";
-//   document.body.classList.remove("disable-scroll");
-//   window.scroll({ top: pagePosition, left: 0 });
-//   document.body.removeAttribute("data-position");
-// };
-
-// openBtn.addEventListener("click", () => {
-//   modal.style.display = "block";
-//   disableScroll();
-// });
-
-// modal.addEventListener("click", (e) => {
-//   if (e.target == modal) {
-//     modal.style.display = "none";
-//     enableScroll();
-//   }
-// });
-
-// closeBtn.addEventListener("click", () => {
-//   modal.style.display = "none";
-//   enableScroll();
-// });
